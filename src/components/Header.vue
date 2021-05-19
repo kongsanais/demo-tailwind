fa3b9476d8c1b47f74f002285db15e10f79f2f80
kongsanais <kongsanais@gmail.com>
2021-05-19 17:51:16 +0700

    kong commit

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="text-center bg-blue-800 text-white p-4 mb-10" >
+   <div class="text-3xl">Covid 19 Tracker</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
